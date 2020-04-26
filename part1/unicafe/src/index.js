a14516b9e03935b543c9c73dded0e00d6cd92b38
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-04-26 17:23:43 +0100

    Solve exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,17 +9,20 @@ const Header = ({ text }) => <h1>{text}</h1>;
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  return (
-    <>
-      <Header text="statistics" />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good - bad) / total}</p>
-      <p>positive {(100 * good) / total}%</p>
-    </>
-  );
+  if (total > 0) {
+    return (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {(good - bad) / total}</p>
+        <p>positive {(100 * good) / total}%</p>
+      </>
+    );
+  } else {
+    return <p>No feedback given</p>;
+  }
 };
 
 const App = () => {
@@ -33,6 +36,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} label="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} label="neutral" />
       <Button handleClick={() => setBad(bad + 1)} label="bad" />
+      <Header text="statistics" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
