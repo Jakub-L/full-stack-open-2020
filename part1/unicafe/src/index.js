51addc843ffe4da9ff36bc58156e22d7e4862c2c
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-04-26 16:32:28 +0100

    Part 1 - Unicafe - Add header component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Header = ({ text }) => <h1>{text}</h1>;
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Header text="statistics" />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
