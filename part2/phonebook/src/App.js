6bc5511834fbea3d2ee3e551af363618c41f32d3
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-05-09 17:09:08 +0100

    Solve exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
     if (persons.some(({ name }) => name === newName)) {
       alert(`${newName} is already in the phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, number: newNumber }]);
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -26,12 +30,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={({ target }) => setNewNumber(target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <p key={name}>{name}</p>
+      {persons.map(({ name, number }) => (
+        <p key={name}>
+          {name} {number}
+        </p>
       ))}
     </div>
   );
