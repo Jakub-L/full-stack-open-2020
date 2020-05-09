483cc83ab6f8c13df0cf5fad13a4fa630a3b4c1a
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2020-05-09 17:01:57 +0100

    Solve exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,30 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addName = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name:
+          <input
+            value={newName}
+            onChange={({ target }) => setNewName(target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(({ name }) => (
+        <p key={name}>{name}</p>
+      ))}
     </div>
   );
 };
